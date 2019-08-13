b49f16330fe8706c7658356acb7bb3b147a9e9d9
unknown <cge.sede@IFC-CGE03.IFRSL.local>
2019-08-13 17:48:46 -0300

    Create Grafo.java

---
+++ Grafo.java
@@ -0,0 +1,23 @@
+package GoogleCharts;
+
+public class Grafo {
+		
+		private int vetor[];
+
+		public int[] getVetor() {
+			return vetor;
+		}
+
+		public void setVetor(int[] vetor) {
+			this.vetor = vetor;
+		}
+		
+//		public Grafico() {
+////			vetor = new int[];
+//		}
+		
+		
+		
+		
+	}
+
